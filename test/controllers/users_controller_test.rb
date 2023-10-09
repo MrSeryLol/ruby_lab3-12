cb287ad8231fecd76bd5420ecafd123e2231df36
MrSeryLol <psygentelmen014@gmail.com>
2023-10-09 19:37:01 +0300

    Exercises for Lab5 is ready

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,5 +4,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get users_new_url
     assert_response :success
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 end
